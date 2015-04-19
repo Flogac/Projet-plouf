9e8e6ec2272b676a6c7cdad7dd740f01bddbad22
William Tchi <tchiikun@hotmail.fr>
2015-04-19 16:12:03 +0200

    Tchi : fprintf function for netlist

--- netlist.c
+++ netlist.c
@@ -1,1 +1,39 @@
 
+void print_netlist(netlist n, char* name){
+    int i,j,k;
+    Reseau r;
+    Point p;
+    Segment s;
+
+    if (!n){
+        perror("print_netlist : your netlist is NULL")
+    }
+    FILE* f = fopen(name,w);
+    if (!f){
+        perror("print_netlist : file cannot be open");
+    }
+
+    fprintf(f, "%d\n", n->NbRes);
+    for (i = 0; i < n->NbRes; i++){
+        r= n->T_Res[i];
+        fprintf(f, "%d %d %d\n",
+                r->NumRes,
+                r->NbPt,
+                r->NbSeg);
+        for (j = 0; j < r->nbPt; j++){
+            p = r->T_Seg[j];
+            fprintf(f, " %d %d %d",
+                    j,
+                    p->x,
+                    p->y);
+        }
+        for(k = 0; k < r->nbSeg; k++){
+            s = r->T_Seg;
+
+            fprintf(" %d %d",
+                    s->p1,
+                    s->p2);
+        }
+    }
+    return;
+}
