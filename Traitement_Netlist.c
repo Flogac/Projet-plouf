c1b13cbdaec9df605053d75e2705adbb57e928de
Flogac <florentgacougnolle@yahoo.fr>
2015-04-20 16:32:39 +0200

    Pseudo code QuickSort

--- Traitement_Netlist.c
+++ Traitement_Netlist.c
@@ -125,4 +125,24 @@ void Sauvegarde_intersection(Segment* *segp, int NbSeg, char* name){
     }
 }
 
+/*
+partitionner(tableau T, entier premier, entier dernier, pivot)
+    échanger T[pivot] et T[dernier]
+    j := premier
+    pour i de premier à dernier - 1
+        si T[i] <= T[dernier] alors
+            échanger T[i] et T[j]
+            j := j + 1
+    échanger T[dernier] et T[j]
+    renvoyer j
 
+tri_rapide(tableau T, entier premier, entier dernier)
+    début
+        si premier < dernier alors
+            pivot := choix_pivot(T, premier, dernier)
+            pivot := partitionner(T, premier, dernier, pivot)
+            tri_rapide(T ,premier, pivot-1)
+            tri_rapide(T, pivot+1, dernier)
+        fin si
+    fin
+*/
