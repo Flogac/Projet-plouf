7e3176f0cde066a474d6c1e52938e648a435a328
Flogac <florentgacougnolle@yahoo.fr>
2015-04-23 18:03:28 +0200

    Main menu

--- main.c
+++ main.c
@@ -3,8 +3,36 @@
 
 int main(){
 
-//VisuNetList();
+int quitter;
+char * choixmenu;
 
+choixmenu = ( char * ) malloc ( sizeof( char ) * 256 );
+printf( "Bienvenue dans le projet de Florent \"Jean-Yves\" Gacougnolle et William \"Jean-Yves\" Fabre.\n" );
 
+quitter = 0;
+while( quitter == 0 ){
+
+    printf( "Menu:\n" );
+    printf( "0_ Informations\n");
+    printf( "1 Programme fini\n");
+    printf( "2 VisuNetList\n");
+    printf( "3 Test intersections\n" );
+    printf( "9 Quitter\n");
+    printf( "Nous allons maintenant procéder à la saisie. Tout caractère superflu ne sera pas pris en compte.");
+    printf( "Tout caractère non proposé sera considéré comme une demande de réécrire le menu.\n");
+
+    scanf( "%s" , choixmenu );
+
+    if( choixmenu[0] == '0' ) printf( "Cette fonctionnalité n'a pas encore été implentée. LOLOLOLOLOLOLOL\n");
+    if( choixmenu[0] == '1' ) printf( "Le programme est en construction.\n" );
+    if( choixmenu[0] == '2' ) VisuNetList();
+    if( choixmenu[0] == '3' ) printf( "C'est pour bientôt.\n");
+    if( choixmenu[0] == '9' ){
+        printf( "C'est si triste de nous quitter si tôt.\n" );
+        quitter = 1;
+    }
+}
+
+free( choixmenu );
 return 0;
 }
